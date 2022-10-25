d70a3c30829e310c60dab56ee6ccd569967fd3c2
Kevinoo5 <kevin.noormets@voco.ee>
2022-10-25 13:31:38 +0300

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 document.addEventListener("DOMContentLoaded", cityWeather)
 
 function weatherDataFetch(city) {
-    var key = "b9b27e0c6774d5bc94269cbec554bdbe"
+    let key = "b9b27e0c6774d5bc94269cbec554bdbe"
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then(function(resp) {
             return resp.json()
         })
         .then(function(data) {
             console.log(data)
+            drawWeather(data)
         })
         .catch(function() {
 
         });
 }
 
-function cityWeather(e) {
+function cityWeather() {
     weatherDataFetch("Tallinn")
+}
+
+function drawWeather(data) {
+    let celsius = Math.round(parseFloat(data.main.temp)-273.15)
+    let description = data.weather[0].description
+
+    document.querySelector("#description").innerHTML = description
+    console.log(celsius + "&deg;")
+    document.querySelector("#temp").innerHTML = celsius + "&deg;"
+    document.querySelector("#location").innerHTML = data.name
 }
